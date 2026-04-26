8319cda8781559b476e5afe9805603cf5e8164d4
Leogahu <leonardo10-gh@hotmail.com>
2026-04-26 15:22:51 -0500

    dashboard lateral funcional

--- backend/src/main/java/com/bolsaempleo/repository/HabilidadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bolsaempleo.repository;
-
-import com.bolsaempleo.model.Habilidad;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface HabilidadRepository extends JpaRepository<Habilidad, Long> {
-    List<Habilidad> findByPostanteId(Long postanteId);
-}
